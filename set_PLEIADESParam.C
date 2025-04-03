98b5cd56fa53fa1d4681b4c5495be77ee2294b35
J. Adamczewski-Musch <j.adamczewski@gsi.de>
2025-04-03 17:07:21 +0200

    provide default trace size in parameter setup

--- set_PLEIADESParam.C
+++ set_PLEIADESParam.C
@@ -180,4 +180,5 @@ void set_PLEIADESParam()
        param0->fCrystalMap[sBPname2][0] = 0x03E;         // BP pin1 goes to card 3, ch 12
        param0->fCrystalMap[sBPname2][1] = 0x03F;         // BP pin2 goes to card 3, ch 13
 
+       param0->fTraceSize = 3000;
 }
